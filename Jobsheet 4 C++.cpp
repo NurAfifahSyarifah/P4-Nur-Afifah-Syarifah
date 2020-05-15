a220cd37773763e4609908a9bd957286aaca4b04
NurAfifahSyarifah <64617653+NurAfifahSyarifah@users.noreply.github.com>
2020-05-15 20:28:07 +0700

    Create Jobsheet 4 C++.cpp
    Pengurangan Matriks 2 dimensi

---
+++ Jobsheet 4 C++.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int i, j, m, n, matriks1[10][10], matriks2[10][10], hasil[10][10];
+  cout << "Masukkan jumlah baris : ";
+  cin >> m;
+  cout << "Masukkan jumlah kolom : ";
+  cin >> n;
+  cout << "Masukkan NIM mahasiswa pertama: \n";
+  for(i = 0; i < m; i++){
+    for(j = 0; j < n; j++){
+      cin >> matriks1[i][j];
+    }
+  }
+  
+  cout << "Masukkan NIM mahasiswa kedua: \n";
+  for(i = 0; i < m; i++){
+    for(j = 0; j < n; j++){
+      cin >> matriks2[i][j];
+    }
+  }
+  
+  cout << "hasil pengurangan matrix: \n";
+  for(i = 0; i < m; i++){
+    for(j = 0; j < n; j++){
+      hasil[i][j] = matriks1[i][j] - matriks2[i][j];
+      cout << hasil[i][j] << "\t";
+    }
+    cout << endl;
+  }
+  
+  return 0;
+}
