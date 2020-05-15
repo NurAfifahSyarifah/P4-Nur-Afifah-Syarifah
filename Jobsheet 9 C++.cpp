023359aa49f7ef43d50d9b4e3b0e921db37535ea
NurAfifahSyarifah <64617653+NurAfifahSyarifah@users.noreply.github.com>
2020-05-15 20:43:52 +0700

    Create Jobsheet 9 C++.cpp
    Array sebagai Karakter

---
+++ Jobsheet 9 C++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+int i;
+char nama[5]={'I','F','A','T'};
+char nama2[6]="IFAT";
+cout<<"Array per Karakter= ";
+for(i=0;i<=4;i++)
+{
+cout<<nama[i];
+}
+cout<<endl; cout<<"Array string ="<<nama2<<endl<<endl;
+return 0;
+}
