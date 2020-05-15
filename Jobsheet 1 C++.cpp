6c6c0a73b67b48c7d6208bcc818646005fb9c561
NurAfifahSyarifah <64617653+NurAfifahSyarifah@users.noreply.github.com>
2020-05-15 20:19:49 +0700

    Create Jobsheet 1 C++.cpp
    Menampilkan array 1  dimensi

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Nur Afifah Syarifah-F1B019-108\n\n";
+	cout<<"Kelompok 17\n\n";
+int nim[18]= {11,21,31,41,51,61,71,81,33,22,11,44,55,66,88,99,23,14};
+	
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+	cout << "Nim ke-6 adalah " << nim[5] << endl;
+	cout << "Nim ke-7 adalah " << nim[6] << endl;
+	cout << "Nim ke-8 adalah " << nim[7] << endl;
+	cout << "Nim ke-9 adalah " << nim[8] << endl;
+	cout << "Nim ke-10adalah " << nim[9] << endl;
+	cout << "Nim ke-11adalah " << nim[10] << endl;
+	cout << "Nim ke-12adalah " << nim[11] << endl;
+	cout << "Nim ke-13adalah " << nim[12] << endl;
+	cout << "Nim ke-14adalah " << nim[13] << endl;
+	cout << "Nim ke-15adalah " << nim[14] << endl;
+	cout << "Nim ke-16adalah " << nim[15]<< endl;
+	cout << "Nim ke-17adalah " << nim[16] << endl;
+	cout << "Nim ke-18adalah " << nim[17] << endl;
+	
+
+}
