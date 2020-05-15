ad8308da9d0c874bc2da0486ab2dff9f6773e403
NurAfifahSyarifah <64617653+NurAfifahSyarifah@users.noreply.github.com>
2020-05-15 20:22:18 +0700

    Create Jobsheet 2 C++.cpp
    Mengisi dan
    menampilkan elemen array
    dinamis

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+main()
+{
+string x[8];
+cout<<"nim mahasiswa pertama = ";
+cin>>x[0];
+cout<<"nim mahasiswa kedua   = ";
+cin>>x[1];
+cout<<"nim mahasiswa ketiga  = ";
+cin>>x[2];
+cout<<"nim mahasiswa keempat = ";
+cin>>x[3];
+cout<<"nim mahasiswa kelima  = ";
+cin>>x[4];
+cout<<"nim mahasiswa keenam  = ";
+cin>>x[5];
+cout<<"nim mahasiswa ketujuh = ";
+cin>>x[6];
+cout<<"nim mahasiswa kedelpn = ";
+cin>>x[7];
+cout <<"nim mahasiswa pertama = "<<x[0]<<"\n";
+cout <<"nim mahasiswa kedua   = "<<x[1]<<"\n";
+cout <<"nim mahasiswa ketiga  = "<<x[2]<<"\n";
+cout <<"nim mahasiswa keempat = "<<x[3]<<"\n";
+cout <<"nim mahasiswa kelima  = "<<x[4]<<"\n";
+cout <<"nim mahasiswa keenam  = "<<x[5]<<"\n";
+cout <<"nim mahasiswa ketujuh = "<<x[6]<<"\n";
+cout <<"nim mahasiswa kedelpn = "<<x[7]<<"\n";
+
+}
